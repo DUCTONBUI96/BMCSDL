74ded803dc20c3f7573bc598ec18cd8c878b1ef7
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-13 18:07:50 +0700

    add and desgin ApprovalForm

---
+++ CuoiKi/ApprovalForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CuoiKi
+{
+    partial class ApprovalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvApplications = new System.Windows.Forms.DataGridView();
+            this.rtbNotes = new System.Windows.Forms.RichTextBox();
+            this.btnApprove = new System.Windows.Forms.Button();
+            this.btnReject = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplications)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvApplications
+            // 
+            this.dgvApplications.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvApplications.Location = new System.Drawing.Point(12, 42);
+            this.dgvApplications.Name = "dgvApplications";
+            this.dgvApplications.RowHeadersWidth = 51;
+            this.dgvApplications.RowTemplate.Height = 24;
+            this.dgvApplications.Size = new System.Drawing.Size(773, 253);
+            this.dgvApplications.TabIndex = 0;
+            // 
+            // rtbNotes
+            // 
+            this.rtbNotes.Location = new System.Drawing.Point(12, 325);
+            this.rtbNotes.Name = "rtbNotes";
+            this.rtbNotes.Size = new System.Drawing.Size(773, 114);
+            this.rtbNotes.TabIndex = 1;
+            this.rtbNotes.Text = "Nhập ghi chú... ";
+            this.rtbNotes.Visible = false;
+            this.rtbNotes.WordWrap = false;
+            // 
+            // btnApprove
+            // 
+            this.btnApprove.Location = new System.Drawing.Point(12, 474);
+            this.btnApprove.Name = "btnApprove";
+            this.btnApprove.Size = new System.Drawing.Size(100, 30);
+            this.btnApprove.TabIndex = 2;
+            this.btnApprove.Text = "Phê duyệt";
+            this.btnApprove.UseVisualStyleBackColor = true;
+            // 
+            // btnReject
+            // 
+            this.btnReject.Location = new System.Drawing.Point(145, 474);
+            this.btnReject.Name = "btnReject";
+            this.btnReject.Size = new System.Drawing.Size(100, 30);
+            this.btnReject.TabIndex = 3;
+            this.btnReject.Text = "Từ chối";
+            this.btnReject.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 516);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "label1";
+            // 
+            // ApprovalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 554);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnReject);
+            this.Controls.Add(this.btnApprove);
+            this.Controls.Add(this.rtbNotes);
+            this.Controls.Add(this.dgvApplications);
+            this.Name = "ApprovalForm";
+            this.Text = "ApprovalForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvApplications)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvApplications;
+        private System.Windows.Forms.RichTextBox rtbNotes;
+        private System.Windows.Forms.Button btnApprove;
+        private System.Windows.Forms.Button btnReject;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
