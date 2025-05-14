cfa2a6685d89c8ce5e3c563ad3501c4a3ce1b1ce
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-14 15:14:37 +0700

    add and design Notification Form

---
+++ CuoiKi/NotificationForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CuoiKi
+{
+    partial class NotificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSendNotification = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 259);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(776, 114);
+            this.txtMessage.TabIndex = 2;
+            this.txtMessage.Text = "Nhập nội dung thông báo...\n";
+            this.txtMessage.WordWrap = false;
+            // 
+            // btnSendNotification
+            // 
+            this.btnSendNotification.Location = new System.Drawing.Point(12, 394);
+            this.btnSendNotification.Name = "btnSendNotification";
+            this.btnSendNotification.Size = new System.Drawing.Size(166, 35);
+            this.btnSendNotification.TabIndex = 3;
+            this.btnSendNotification.Text = "Gửi thông báo";
+            this.btnSendNotification.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nội dung thông báo";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 171);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // NotificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendNotification);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "NotificationForm";
+            this.Text = "NotificationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Button btnSendNotification;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ CuoiKi/NotificationForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuoiKi
+{
+    public partial class NotificationForm : Form
+    {
+        public NotificationForm()
+        {
+            InitializeComponent();
+            txtMessage.ForeColor = Color.Gray;
+            txtMessage.GotFocus += RemovePlaceholder; 
+            txtMessage.LostFocus += SetPlaceholder;
+        }
+
+        //làm mờ nội dung textbox
+        private void RemovePlaceholder(object sender, EventArgs e)
+        {
+            if (txtMessage.ForeColor == Color.Gray)
+            {
+                txtMessage.Text = "";
+                txtMessage.ForeColor = Color.Black;
+            }
+        }
+
+        private void SetPlaceholder(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                txtMessage.Text = "Nhập nội dung thông báo...";
+                txtMessage.ForeColor = Color.Gray;
+            }
+        }
+
+
+
+    }
+}
