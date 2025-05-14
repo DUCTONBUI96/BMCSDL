401352a09c7c4efcb4e0288ed0d71bf2712fb039
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-14 15:03:12 +0700

    add and design AuditTrailForm

---
+++ CuoiKi/AuditTrailForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CuoiKi
+{
+    partial class AuditTrailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.Label();
+            this.cboTableName = new System.Windows.Forms.ComboBox();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dgvLogs = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bảng:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Từ ngày:";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.AutoSize = true;
+            this.dtpFrom.Location = new System.Drawing.Point(458, 42);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(67, 16);
+            this.dtpFrom.TabIndex = 2;
+            this.dtpFrom.Text = "Đến ngày:";
+            // 
+            // cboTableName
+            // 
+            this.cboTableName.FormattingEnabled = true;
+            this.cboTableName.Location = new System.Drawing.Point(62, 39);
+            this.cboTableName.Name = "cboTableName";
+            this.cboTableName.Size = new System.Drawing.Size(121, 24);
+            this.cboTableName.TabIndex = 3;
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CustomFormat = "dd/mm/yyyy";
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpStart.Location = new System.Drawing.Point(276, 42);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(155, 22);
+            this.dtpStart.TabIndex = 4;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.CustomFormat = "dd/mm/yyyy";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(531, 41);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(155, 22);
+            this.dtpTo.TabIndex = 5;
+            // 
+            // dgvLogs
+            // 
+            this.dgvLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogs.Location = new System.Drawing.Point(12, 142);
+            this.dgvLogs.Name = "dgvLogs";
+            this.dgvLogs.RowHeadersWidth = 51;
+            this.dgvLogs.RowTemplate.Height = 24;
+            this.dgvLogs.Size = new System.Drawing.Size(776, 283);
+            this.dgvLogs.TabIndex = 7;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(675, 82);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(113, 40);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // AuditTrailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dgvLogs);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.cboTableName);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AuditTrailForm";
+            this.Text = "AuditTrailForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label dtpFrom;
+        private System.Windows.Forms.ComboBox cboTableName;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DataGridView dgvLogs;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
