614b763f6ccd2a8a64d79ed3e81f37f5b6c66b5e
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-14 14:32:53 +0700

    add and design LogForm

---
+++ CuoiKi/LogForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CuoiKi
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.dgvLogs = new System.Windows.Forms.DataGridView();
+            this.btnFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUserFilter
+            // 
+            this.txtUserFilter.Location = new System.Drawing.Point(87, 35);
+            this.txtUserFilter.Name = "txtUserFilter";
+            this.txtUserFilter.Size = new System.Drawing.Size(148, 22);
+            this.txtUserFilter.TabIndex = 0;
+            this.txtUserFilter.Text = "Tên người dùng...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Người dùng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Từ ngày:";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.CustomFormat = "dd/mm/yyyy";
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpStart.Location = new System.Drawing.Point(364, 41);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(155, 22);
+            this.dtpStart.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(540, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Đến ngày:";
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.CustomFormat = "dd/mm/yyyy";
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpEnd.Location = new System.Drawing.Point(613, 41);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(155, 22);
+            this.dtpEnd.TabIndex = 5;
+            // 
+            // dgvLogs
+            // 
+            this.dgvLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogs.Location = new System.Drawing.Point(12, 155);
+            this.dgvLogs.Name = "dgvLogs";
+            this.dgvLogs.RowHeadersWidth = 51;
+            this.dgvLogs.RowTemplate.Height = 24;
+            this.dgvLogs.Size = new System.Drawing.Size(776, 283);
+            this.dgvLogs.TabIndex = 6;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(675, 89);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(113, 40);
+            this.btnFilter.TabIndex = 7;
+            this.btnFilter.Text = "Lọc";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            // 
+            // LogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.dgvLogs);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserFilter);
+            this.Name = "LogForm";
+            this.Text = "LogForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.DataGridView dgvLogs;
+        private System.Windows.Forms.Button btnFilter;
+    }
+}
