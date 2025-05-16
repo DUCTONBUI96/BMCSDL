a0e4a020295579334c6faf8a49826f219466d24b
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-16 19:52:22 +0700

    done backend ApplicationForm

--- CuoiKi/ApplicationForm.cs
+++ CuoiKi/ApplicationForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
@@ -20,10 +21,37 @@ namespace CuoiKi
           
 
         }
- 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            //chuyen lenh sang string rồi dùng using gọi lại
+            string connectionString = "Data Source=.;Initial Catalog=PassportManagement;Integrated Security=True";
+            string query = "INSERT INTO ResidentData (FullName, Gender, DateOfBirth, CMND, Address, Nationality, PhoneNumber, Email) " +
+                           "VALUES (@FullName, @Gender, @DateOfBirth, @CMND, @Address, @Nationality, @PhoneNumber, @Email)";
 
+            //test connect database
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@FullName", txtFullName.Text);
+                    cmd.Parameters.AddWithValue("@Gender", cboGender.Text);
+                    cmd.Parameters.AddWithValue("@DateOfBirth", dtpDOB.Value);
+                    cmd.Parameters.AddWithValue("@CMND", txtCCCDid.Text);
+                    cmd.Parameters.AddWithValue("@Address", rtbAddress.Text);
+                    cmd.Parameters.AddWithValue("@Nationality", cboNationality.Text);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", txtPhone.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Đăng ký thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đăng ký: " + ex.Message);
+            }
         }
     }
 }
