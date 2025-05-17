880d5fcfe7d5496db8fa8bda26bc077b24212804
quochuy <buiquochuy08122003@gmail.com>
2025-05-17 18:53:34 +0700

    VerificationForm Done

--- CuoiKi/Program.cs
+++ CuoiKi/Program.cs
@@ -16,7 +16,7 @@ namespace CuoiKi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ResidentListForm());
+            Application.Run(new VerificationForm());
         }
     }
 }
--- CuoiKi/VerificationForm.Designer.cs
+++ CuoiKi/VerificationForm.Designer.cs
@@ -28,18 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerificationForm));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.txtSearchCCCD = new System.Windows.Forms.TextBox();
             this.btnApprove = new System.Windows.Forms.Button();
             this.btnReject = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnViewDetails = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
@@ -48,31 +52,48 @@
             // 
             // txtSearchCCCD
             // 
-            this.txtSearchCCCD.Location = new System.Drawing.Point(12, 37);
+            this.txtSearchCCCD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSearchCCCD.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchCCCD.ForeColor = System.Drawing.Color.LightGray;
+            this.txtSearchCCCD.Location = new System.Drawing.Point(15, 16);
+            this.txtSearchCCCD.Multiline = true;
             this.txtSearchCCCD.Name = "txtSearchCCCD";
-            this.txtSearchCCCD.Size = new System.Drawing.Size(200, 22);
+            this.txtSearchCCCD.Size = new System.Drawing.Size(200, 29);
             this.txtSearchCCCD.TabIndex = 1;
-            this.txtSearchCCCD.Text = "Tìm Kiếm";
+            this.txtSearchCCCD.Text = "Tìm Kiếm CCCD";
+            this.txtSearchCCCD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtSearchCCCD.TextChanged += new System.EventHandler(this.txtSearchCCCD_TextChanged);
             // 
             // btnApprove
             // 
-            this.btnApprove.Location = new System.Drawing.Point(12, 368);
+            this.btnApprove.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnApprove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApprove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApprove.Image = ((System.Drawing.Image)(resources.GetObject("btnApprove.Image")));
+            this.btnApprove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnApprove.Location = new System.Drawing.Point(244, 368);
             this.btnApprove.Name = "btnApprove";
-            this.btnApprove.Size = new System.Drawing.Size(100, 35);
+            this.btnApprove.Size = new System.Drawing.Size(126, 35);
             this.btnApprove.TabIndex = 2;
             this.btnApprove.Text = "Xác thực";
-            this.btnApprove.UseVisualStyleBackColor = true;
+            this.btnApprove.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnApprove.UseVisualStyleBackColor = false;
             this.btnApprove.Click += new System.EventHandler(this.btnApprove_Click);
             // 
             // btnReject
             // 
-            this.btnReject.Location = new System.Drawing.Point(138, 368);
+            this.btnReject.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnReject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReject.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReject.Image = ((System.Drawing.Image)(resources.GetObject("btnReject.Image")));
+            this.btnReject.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReject.Location = new System.Drawing.Point(467, 368);
             this.btnReject.Name = "btnReject";
-            this.btnReject.Size = new System.Drawing.Size(100, 35);
+            this.btnReject.Size = new System.Drawing.Size(122, 35);
             this.btnReject.TabIndex = 3;
             this.btnReject.Text = "Từ chối";
-            this.btnReject.UseVisualStyleBackColor = true;
+            this.btnReject.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnReject.UseVisualStyleBackColor = false;
             this.btnReject.Click += new System.EventHandler(this.btnReject_Click);
             // 
             // lblResult
@@ -84,11 +105,39 @@
             this.lblResult.Size = new System.Drawing.Size(0, 16);
             this.lblResult.TabIndex = 4;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnViewDetails
+            // 
+            this.btnViewDetails.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnViewDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewDetails.Image = ((System.Drawing.Image)(resources.GetObject("btnViewDetails.Image")));
+            this.btnViewDetails.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnViewDetails.Location = new System.Drawing.Point(15, 368);
+            this.btnViewDetails.Name = "btnViewDetails";
+            this.btnViewDetails.Size = new System.Drawing.Size(150, 35);
+            this.btnViewDetails.TabIndex = 6;
+            this.btnViewDetails.Text = "Xem chi tiết";
+            this.btnViewDetails.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnViewDetails.UseVisualStyleBackColor = false;
+            // 
             // VerificationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnViewDetails);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnReject);
             this.Controls.Add(this.btnApprove);
@@ -98,6 +147,7 @@
             this.Text = "VerificationForm";
             this.Load += new System.EventHandler(this.VerificationForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,5 +160,7 @@
         private System.Windows.Forms.Button btnApprove;
         private System.Windows.Forms.Button btnReject;
         private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnViewDetails;
     }
 }
