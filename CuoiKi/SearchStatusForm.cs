136ec832ec93df130a9655f70e53eb6b48dad4c2
DUCTONBUI96 <ductonbui46@gmail.com>
2025-05-22 19:59:11 +0700

    done backend SearchStatusForm

--- CuoiKi/SearchStatusForm.cs
+++ CuoiKi/SearchStatusForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,10 +19,14 @@ namespace CuoiKi
             this.txtCCCD.Size=new Size(661, 50);
 
         }
-
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=PassportManagement;Integrated Security=True");
+        DataTable dt = new DataTable();
+        string status = "";
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
+            connectToDatabase();
+            lblResult.Text = "Trạng thái hồ sơ của bạn là: " + status;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -29,10 +34,41 @@ namespace CuoiKi
 
         }
 
+        private void connectToDatabase()
+        {
+            
+            // Kết nối đến cơ sở dữ liệu SQL Server
+            string query = "SELECT Status FROM PassportApplications Where ResidentID IN (SELECT ResidentID FROM ResidentData WHERE CMND = @CMND )";
+            // Tạo kết nối và thực thi truy vấn
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@CMND", txtCCCD.Text);
+                    object result = cmd.ExecuteScalar();
+                    if (result != DBNull.Value)
+                    {
+                        status = result.ToString();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tìm thấy hồ sơ với ID cư trú này.");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối cơ sở dữ liệu: " + ex.Message);
+            }
+        }
         private void SearchStatusForm_Load(object sender, EventArgs e)
         {
-           txtCCCD.Text = "Tên người dùng";
+            txtCCCD.Text = "Tên người dùng";
             txtCCCD.ForeColor = Color.Gray;
+            
         }
 
         private void txtCCCD_Enter(object sender, EventArgs e)
