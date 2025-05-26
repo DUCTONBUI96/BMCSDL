aa7a6f47c1ed37d643bd61c08d9fac78ba1beacf
quochuy <buiquochuy08122003@gmail.com>
2025-05-26 14:03:41 +0700

    Pass

--- CuoiKi/InformationUser.Designer.cs
+++ CuoiKi/InformationUser.Designer.cs
@@ -30,12 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InformationUser));
             this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtConfirmPass = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtName = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -44,40 +40,29 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnCreate = new System.Windows.Forms.Button();
+            this.ckbViewPass2 = new System.Windows.Forms.CheckBox();
+            this.ckbviewPass = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtConfirmPass = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEmail.Location = new System.Drawing.Point(352, 259);
+            this.txtEmail.Location = new System.Drawing.Point(352, 133);
             this.txtEmail.Multiline = true;
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(236, 28);
             this.txtEmail.TabIndex = 40;
             // 
-            // txtConfirmPass
-            // 
-            this.txtConfirmPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtConfirmPass.Location = new System.Drawing.Point(352, 198);
-            this.txtConfirmPass.Multiline = true;
-            this.txtConfirmPass.Name = "txtConfirmPass";
-            this.txtConfirmPass.Size = new System.Drawing.Size(236, 28);
-            this.txtConfirmPass.TabIndex = 38;
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(352, 146);
-            this.txtPassword.Multiline = true;
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(236, 28);
-            this.txtPassword.TabIndex = 37;
-            // 
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(352, 89);
+            this.txtName.Location = new System.Drawing.Point(352, 69);
             this.txtName.Multiline = true;
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(236, 30);
@@ -87,37 +72,18 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(104, 259);
+            this.label6.Location = new System.Drawing.Point(104, 139);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(54, 22);
             this.label6.TabIndex = 35;
             this.label6.Text = "Email";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(104, 196);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(164, 22);
-            this.label4.TabIndex = 33;
-            this.label4.Text = "Xác nhận mật khẩu";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(104, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 22);
-            this.label3.TabIndex = 32;
-            this.label3.Text = "Mật khẩu";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(104, 97);
+            this.label2.Location = new System.Drawing.Point(104, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 22);
             this.label2.TabIndex = 31;
@@ -128,7 +94,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(322, 33);
+            this.label1.Location = new System.Drawing.Point(300, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(288, 38);
             this.label1.TabIndex = 30;
@@ -138,7 +104,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(104, 322);
+            this.label5.Location = new System.Drawing.Point(100, 197);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(156, 22);
             this.label5.TabIndex = 45;
@@ -153,7 +119,7 @@
             "Xét duyệt (XD)",
             "Lưu trữ (LT)",
             "Giám sát (GS)"});
-            this.cbbVaitro.Location = new System.Drawing.Point(352, 320);
+            this.cbbVaitro.Location = new System.Drawing.Point(352, 191);
             this.cbbVaitro.Name = "cbbVaitro";
             this.cbbVaitro.Size = new System.Drawing.Size(236, 28);
             this.cbbVaitro.TabIndex = 46;
@@ -162,7 +128,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(273, 33);
+            this.pictureBox1.Location = new System.Drawing.Point(251, 9);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(54, 46);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -176,7 +142,7 @@
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.Image = ((System.Drawing.Image)(resources.GetObject("btnBack.Image")));
             this.btnBack.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBack.Location = new System.Drawing.Point(575, 383);
+            this.btnBack.Location = new System.Drawing.Point(575, 404);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(121, 34);
             this.btnBack.TabIndex = 43;
@@ -191,7 +157,7 @@
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
             this.btnClear.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClear.Location = new System.Drawing.Point(352, 383);
+            this.btnClear.Location = new System.Drawing.Point(352, 404);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(120, 34);
             this.btnClear.TabIndex = 42;
@@ -206,7 +172,7 @@
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.Image = ((System.Drawing.Image)(resources.GetObject("btnCreate.Image")));
             this.btnCreate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCreate.Location = new System.Drawing.Point(134, 383);
+            this.btnCreate.Location = new System.Drawing.Point(134, 404);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(122, 34);
             this.btnCreate.TabIndex = 41;
@@ -214,12 +180,92 @@
             this.btnCreate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnCreate.UseVisualStyleBackColor = false;
             // 
+            // ckbViewPass2
+            // 
+            this.ckbViewPass2.AutoSize = true;
+            this.ckbViewPass2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbViewPass2.Location = new System.Drawing.Point(450, 372);
+            this.ckbViewPass2.Name = "ckbViewPass2";
+            this.ckbViewPass2.Size = new System.Drawing.Size(138, 24);
+            this.ckbViewPass2.TabIndex = 56;
+            this.ckbViewPass2.Text = "Xem mật khẩu";
+            this.ckbViewPass2.UseVisualStyleBackColor = true;
+            this.ckbViewPass2.CheckedChanged += new System.EventHandler(this.ckbViewPass2_CheckedChanged);
+            // 
+            // ckbviewPass
+            // 
+            this.ckbviewPass.AutoSize = true;
+            this.ckbviewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbviewPass.Location = new System.Drawing.Point(450, 288);
+            this.ckbviewPass.Name = "ckbviewPass";
+            this.ckbviewPass.Size = new System.Drawing.Size(138, 24);
+            this.ckbviewPass.TabIndex = 55;
+            this.ckbviewPass.Text = "Xem mật khẩu";
+            this.ckbviewPass.UseVisualStyleBackColor = true;
+            this.ckbviewPass.CheckedChanged += new System.EventHandler(this.ckbviewPass_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(453, 304);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 22);
+            this.label7.TabIndex = 54;
+            // 
+            // txtConfirmPass
+            // 
+            this.txtConfirmPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPass.Location = new System.Drawing.Point(352, 338);
+            this.txtConfirmPass.Multiline = true;
+            this.txtConfirmPass.Name = "txtConfirmPass";
+            this.txtConfirmPass.PasswordChar = '*';
+            this.txtConfirmPass.Size = new System.Drawing.Size(236, 28);
+            this.txtConfirmPass.TabIndex = 53;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(352, 254);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(236, 28);
+            this.txtPassword.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 22);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Xác nhận mật khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 22);
+            this.label3.TabIndex = 50;
+            this.label3.Text = "Mật khẩu";
+            // 
             // InformationUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ckbViewPass2);
+            this.Controls.Add(this.ckbviewPass);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtConfirmPass);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.cbbVaitro);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.pictureBox1);
@@ -227,12 +273,8 @@
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtConfirmPass);
-            this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "InformationUser";
@@ -250,15 +292,18 @@
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtConfirmPass;
-        private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox cbbVaitro;
+        private System.Windows.Forms.CheckBox ckbViewPass2;
+        private System.Windows.Forms.CheckBox ckbviewPass;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtConfirmPass;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
--- CuoiKi/InformationUser.cs
+++ CuoiKi/InformationUser.cs
@@ -15,11 +15,28 @@ namespace CuoiKi
         public InformationUser()
         {
             InitializeComponent();
+            txtPassword.UseSystemPasswordChar = true;
+            txtConfirmPass.UseSystemPasswordChar = true;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ckbviewPass_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !ckbviewPass.Checked;
+        }
+
+        private void ckbViewPass2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtConfirmPass.UseSystemPasswordChar = !ckbViewPass2.Checked;
         }
     }
 }
